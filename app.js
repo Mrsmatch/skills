68d0ac03140e5b275d8b4bb7705c909c739a73b2
name <1409697119@qq.com>
2018-11-07 10:55:55 +0800

    哈哈v

---
+++ app.js
@@ -0,0 +1,14 @@
+function require() {
+    bauseUrl: '/src';
+    paths: {
+        'html',
+        './src/html/index.html';
+
+    };
+    shim: {}
+
+}
+
+function define() {
+    define(['haha', '$', 'kk'])
+}
